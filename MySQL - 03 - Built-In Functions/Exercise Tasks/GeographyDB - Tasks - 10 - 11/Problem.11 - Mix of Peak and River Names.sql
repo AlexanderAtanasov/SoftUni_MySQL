faa68edae5d52e752b2e3a4ab45bb7f402d50d58
AlexanderAtanasov <79150895+AlexanderAtanasov@users.noreply.github.com>
2021-06-07 14:53:29 +0300

    Add files via upload

---
+++ MySQL - 03 - Built-In Functions/Exercise Tasks/GeographyDB - Tasks - 10 - 11/Problem.11 - Mix of Peak and River Names.sql
@@ -0,0 +1,5 @@
+SELECT `peak_name`, `river_name`, 
+lower(CONCAT(`peak_name`,substring(`river_name`,2))) AS 'mix'
+FROM `peaks`,`rivers`
+WHERE right(`peak_name`,1)=left(`river_name`,1)
+ORDER BY `mix`;
