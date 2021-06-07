1b0fc40514134ed36716dde1e3fc50dd76dc15ec
AlexanderAtanasov <79150895+AlexanderAtanasov@users.noreply.github.com>
2021-06-07 15:48:25 +0300

    Update Problem.15 - Show All Games with Duration and Part of the Day.sql

--- MySQL - 03 - Built-In Functions/Exercise Tasks/DiabloDB - Tasks - 12 - 15/Problem.15 - Show All Games with Duration and Part of the Day.sql
+++ MySQL - 03 - Built-In Functions/Exercise Tasks/DiabloDB - Tasks - 12 - 15/Problem.15 - Show All Games with Duration and Part of the Day.sql
@@ -1,7 +1,16 @@
-SELECT `name`,
-IF(hour(`start`) BETWEEN 0 AND 11 ,'Morning',
-IF(hour(`start`) BETWEEN 12 AND 17 , 'Afternoon','Evening') 
-) AS 'Part of the Day' ,
-IF(`duration` <= 3, 'Extra Short',
-IF(`duration` BETWEEN 4 AND 6,'Short',IF(`duration` between 7 and 10,'Long','Extra Long'))) AS 'Duration'
-FROM games;
+SELECT 
+    `name`,
+    CASE
+		WHEN HOUR(`start`) BETWEEN 0 AND 11 THEN 'Morning'
+		WHEN HOUR(`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
+		ELSE 'Evening'
+    END AS 'Part of the Day',
+	CASE 
+		WHEN `duration` <= 3 THEN 'Extra Short'
+		WHEN `duration` BETWEEN 4 AND 6 THEN 'Short'
+		WHEN `duration` BETWEEN 7 AND 10 THEN 'Long'
+        ELSE 'Extra Long' 
+   END AS 'Duration'
+FROM
+    games;
+    
